7638e5d0c1206855eb3d22217f41024ebf9d31bd
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2023-05-14 20:45:05 +0200

    added theme selection

---
+++ pages/components/ThemeScreen.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useRef, useState} from "react";
+import io from 'socket.io-client';
+
+const socket = io("localhost:3000");
+
+function ThemeScreen() {
+
+    return (
+        <section id={"themeScreen"} className={"hide"}>
+
+            <h1>Selection du thème en cours</h1>
+
+        </section>
+    )
+}
+
+export default ThemeScreen;
