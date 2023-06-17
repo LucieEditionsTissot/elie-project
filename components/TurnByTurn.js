d1191428f42c4e717cab76fa08c51e06a0dc8609
Lucie <llesnier@editions-tissot.fr>
2023-06-17 18:09:40 +0200

    turn by turn problem, try with ref but undefined on teamMembers

--- components/TurnByTurn.js
+++ components/TurnByTurn.js
@@ -1,12 +1,11 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from 'socket.io-client';
-import {randomBytes} from "crypto";
-import {pick} from "next/dist/lib/pick";
+import { randomBytes } from "crypto";
+import { pick } from "next/dist/lib/pick";
 
-const socket = io('localhost:3000')
+const socket = io('localhost:3000');
 
 function TurnByTurn(props) {
-
     const [stateOfTheGame, setStateOfTheGame] = useState(null);
     const [actualIndexOfMembers, setActualIndexOfMembers] = useState(0);
     const [maxNumberOfCard, setMaxNumberOfCard] = useState(3);
@@ -22,36 +21,46 @@ function TurnByTurn(props) {
     const [correctAnswer, setCorrectAnswer] = useState("");
     const [isValueSubmit, setIsValueSubmit] = useState(false);
 
+    const elementRef = useRef(null);
+    const allCardsRef = useRef([]);
+    const allHiddenCardsRef = useRef([]);
+    const validateButtonRef = useRef(null);
+    const waitingScreenRef = useRef(null);
+    const waitingScreenTextRef = useRef(null);
+    const timerWrapperRef = useRef(null);
+    const timerRef = useRef(null);
+    const answerTextRef = useRef(null);
+
     useEffect(() => {
-        setData(props.data)
-        setTeams(props.data[0])
-        setRandomTheme(props.data[3])
-        setTeamIndex(props.data[Number(props.client)])
-        const animalData = props.data[4]
+        setData(props.data);
+        setTeams(props.data[0]);
+        setRandomTheme(props.data[3]);
+        setTeamIndex(props.data[Number(props.client)]);
+        const animalData = props.data[4];
         if (animalData) {
-            setAnimals(animalData[props.groupName]["animals"])
-            setCorrectAnswer(animalData[props.groupName]["answer"])
+            setAnimals(animalData[props.groupName]["animals"]);
+            setCorrectAnswer(animalData[props.groupName]["answer"]);
         }
-    }, [props.data])
+    }, [props.data]);
 
     useEffect(() => {
         if (teams && teamIndex !== null) {
             setActualTeamName(Object.keys(teams)[teamIndex]);
             setActualTeamMembers(Object.values(teams)[teamIndex]);
-            setStateOfTheGame(0)
+            setStateOfTheGame(0);
         }
     }, [teams, teamIndex]);
 
     useEffect(() => {
         if (stateOfTheGame !== null) {
-            showTipsWaitingScreen("Indice en cours !")
+            showTipsWaitingScreen("Indice en cours !");
         }
     }, [stateOfTheGame]);
 
     function handleFlipCard(e) {
-        const element = e.target.closest('.animal')
-        const allCards = document.querySelectorAll(".animal");
-        let allHiddenCards = document.querySelectorAll(".animal.hidden");
+        const element = e.target.closest('.animal');
+        const allCards = allCardsRef.current;
+        let allHiddenCards = allHiddenCardsRef.current;
 
         if (!isValueSubmit) {
             if (allHiddenCards.length < maxNumberOfCard) {
@@ -63,47 +72,44 @@ function TurnByTurn(props) {
             }
         }
 
-        allHiddenCards = document.querySelectorAll(".animal.hidden")
-        const validateButton = document.querySelector("#turnByTurn .validateButton")
+        allHiddenCards = Array.from(elementRef.current.getElementsByClassName("animal hidden"));
+        const validateButton = validateButtonRef.current;
 
         if (allHiddenCards.length === Object.keys(animals).length - 1) {
-            validateButton.style.display = "block"
+            validateButton.style.display = "block";
         } else {
-            validateButton.style.display = "none"
+            validateButton.style.display = "none";
         }
-
     }
 
     function showTipsWaitingScreen(text) {
-        const waitingScreen = document.querySelector(".waitingScreen")
-        const waitingScreenText = document.querySelector(".waitingScreen h4")
-        waitingScreen.classList.add("is-active")
-        waitingScreenText.innerHTML = text
+        const waitingScreen = waitingScreenRef.current;
+        const waitingScreenText = waitingScreenTextRef.current;
+        waitingScreen.classList.add("is-active");
+        waitingScreenText.innerHTML = text;
 
-        console.log("stateOfTheGame", stateOfTheGame)
+        console.log("stateOfTheGame", stateOfTheGame);
 
         setTimeout(() => {
-            showPlayerWaitingScreen()
-        }, 3000)
+            showPlayerWaitingScreen();
+        }, 3000);
     }
 
     function showPlayerWaitingScreen() {
-
-        const waitingScreen = document.querySelector(".waitingScreen")
-        const waitingScreenText = document.querySelector(".waitingScreen h4")
-        waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers] + " à toi de jouer !"
-        waitingScreen.classList.add("is-active")
+        const waitingScreen = waitingScreenRef.current;
+        const waitingScreenText = waitingScreenTextRef.current;
+        waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers] + " à toi de jouer !";
+        waitingScreen.classList.add("is-active");
 
         setTimeout(() => {
-            waitingScreen.classList.remove("is-active")
-            pickPhaseAndUpdateDependencies()
-        }, 3000)
-
+            waitingScreen.classList.remove("is-active");
+            pickPhaseAndUpdateDependencies();
+        }, 3000);
     }
 
     function pickPhaseAndUpdateDependencies() {
-        const timerWrapper = document.querySelector(".timer-wrapper");
-        const timer = document.querySelector(".timer");
+        const timerWrapper = timerWrapperRef.current;
+        const timer = timerRef.current;
 
         if (stateOfTheGame !== null) {
             timer.style.animationDuration = `${globalTimer}s`;
@@ -113,75 +119,69 @@ function TurnByTurn(props) {
         setTimeout(() => {
             timer.style.animationPlayState = "paused";
             if (stateOfTheGame < 1) {
-                setStateOfTheGame(stateOfTheGame + 1)
-                setActualIndexOfMembers(actualIndexOfMembers + 1)
+                setStateOfTheGame(stateOfTheGame + 1);
+                setActualIndexOfMembers(actualIndexOfMembers + 1);
                 if (maxNumberOfCard < 9) {
-                    setMaxNumberOfCard(maxNumberOfCard + 3)
+                    setMaxNumberOfCard(maxNumberOfCard + 3);
                 }
             } else {
-                setActualIndexOfMembers(actualIndexOfMembers + 1)
-                updateWaitingScreenForTheLastTime()
-                disableTimer()
-                setMaxNumberOfCard(maxNumberOfCard + 3)
+                setActualIndexOfMembers(actualIndexOfMembers + 1);
+                updateWaitingScreenForTheLastTime();
+                disableTimer();
+                setMaxNumberOfCard(maxNumberOfCard + 3);
             }
         }, globalTimer * 1000);
-
     }
 
     function updateWaitingScreenForTheLastTime() {
-
-        const waitingScreen = document.querySelector(".waitingScreen")
-        const waitingScreenText = document.querySelector(".waitingScreen h4")
-        waitingScreenText.innerHTML = "Indice en cours !"
-        waitingScreen.classList.add("is-active")
+        const waitingScreen = waitingScreenRef.current;
+        const waitingScreenText = waitingScreenTextRef.current;
+        waitingScreenText.innerHTML = "Indice en cours !";
+        waitingScreen.classList.add("is-active");
 
         setTimeout(() => {
             if (actualTeamMembers[actualIndexOfMembers + 1]) {
-                waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers + 1] + " à toi de jouer !"
+                waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers + 1] + " à toi de jouer !";
             } else {
-                waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers] + " à toi de jouer !"
+                waitingScreenText.innerHTML = actualTeamMembers[actualIndexOfMembers] + " à toi de jouer !";
             }
             setTimeout(() => {
-                waitingScreen.classList.remove("is-active")
-            }, 3000)
-        }, 3000)
-
+                waitingScreen.classList.remove("is-active");
+            }, 3000);
+        }, 3000);
     }
 
     function disableTimer() {
-        const timerWrapper = document.querySelector(".timer-wrapper");
-        const timer = document.querySelector(".timer");
+        const timerWrapper = timerWrapperRef.current;
+        const timer = timerRef.current;
         timerWrapper.style.display = "none";
         timer.style.display = "none";
         timer.style.animationPlayState = "paused";
     }
 
     function handleClickOnValidateButton() {
-        const lastCard = document.querySelectorAll(".animal:not(.hidden)")
-        const answerText = document.querySelector(".answerText")
+        const lastCard = Array.from(elementRef.current.getElementsByClassName("animal:not(.hidden)"));
+        const answerText = answerTextRef.current;
         if (lastCard.length === 1 && isValueSubmit === false) {
-            setIsValueSubmit(true)
-            socket.emit("animalChosen", Number(lastCard.id))
+            setIsValueSubmit(true);
+            socket.emit("animalChosen", Number(lastCard[0].id));
             if (Number(lastCard[0].id) === Number(correctAnswer)) {
-                answerText.innerHTML = "Bonne réponse !"
+                answerText.innerHTML = "Bonne réponse !";
             } else {
-                answerText.innerHTML = "Mauvaise réponse !"
+                answerText.innerHTML = "Mauvaise réponse !";
             }
         }
-
     }
 
     return (
-        <section id={"turnByTurn"} className={"hide"}>
-
+        <section id="turnByTurn" className="hide">
             <h1>Équipe {actualTeamName}</h1>
 
             {actualTeamMembers !== undefined && actualTeamMembers.length > 0 && stateOfTheGame !== null ? (
                 <h2>A toi de jouer {actualTeamMembers[actualIndexOfMembers]} !</h2>
             ) : null}
 
-            <div className="animal-wrapper">
-
+            <div className="animal-wrapper" ref={elementRef}>
                 {animals !== undefined && animals.length > 0 ? (
                     animals.map((animal, index) => (
                         <div
@@ -196,22 +196,19 @@ function TurnByTurn(props) {
                 ) : null}
             </div>
 
-            <div className={"timer-wrapper"}>
-                <div className={"timer"}></div>
+            <div className="timer-wrapper" ref={timerWrapperRef}>
+                <div className="timer" ref={timerRef}></div>
             </div>
 
-            <div className={"validateButton"} onClick={() => handleClickOnValidateButton()}>
+            <div className="validateButton" onClick={handleClickOnValidateButton} ref={validateButtonRef}>
                 <p>Validate</p>
             </div>
 
-            <h5 className={"answerText"}></h5>
+            <h5 className="answerText" ref={answerTextRef}></h5>
 
-            <div className={"waitingScreen"}>
-
-                <h4>Premier indice en cours</h4>
-
+            <div className="waitingScreen" ref={waitingScreenRef}>
+                <h4 ref={waitingScreenTextRef}>Premier indice en cours</h4>
             </div>
-
         </section>
     );
 }
